5a78873037c2b9bd410008aba7b30cbbd394c256
bekhruz <bekhruzabdullakhujaevpro@gmail.com>
2023-07-18 11:24:23 +0500

    SpeedometerLikeProgress + Lottie animation

--- app/src/main/java/com/example/szfirstapplication/activities/MainActivity.kt
+++ app/src/main/java/com/example/szfirstapplication/activities/MainActivity.kt
@@ -2,22 +2,29 @@ package com.example.szfirstapplication.activities
 
 import android.content.res.Configuration
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.szfirstapplication.composables.LimitCharLength
+import androidx.compose.ui.unit.sp
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.rememberLottieComposition
+import com.example.szfirstapplication.R
 import com.example.szfirstapplication.composables.PersonLazyColumn
+import com.example.szfirstapplication.composables.SpeedometerLikeProgress
 import com.example.szfirstapplication.data.repositories.PersonRepository
 import com.example.szfirstapplication.ui.theme.SzFirstApplicationTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -27,26 +34,38 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            var name by remember { mutableStateOf("") }
             SzFirstApplicationTheme {
                 Surface(
                     modifier = Modifier
                         .fillMaxSize()
                         .background(MaterialTheme.colors.background)
                 ) {
+                    val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.lottie_fast_move))
+                    var isLottiePlaying by remember { mutableStateOf(false) }
+
+                    LottieAnimation(composition = composition, modifier = Modifier.fillMaxSize(), isPlaying = isLottiePlaying, iterations = 7, speed = 1.5f)
                     Column(
                         horizontalAlignment = Alignment.CenterHorizontally,
                         verticalArrangement = Arrangement.Center,
-                        modifier = Modifier
-                            .padding(24.dp)
+                        modifier = Modifier.padding(24.dp)
                     ) {
-                        LimitCharLength(name) { newName -> name = newName }
-                        Spacer(modifier = Modifier.height(16.dp))
-                        Button(onClick = {
-                            Toast.makeText(this@MainActivity, "$name is sent!",Toast.LENGTH_SHORT).show()
-                        }) {
-                            Text("Send name")
-                        }
+                        var speed by remember { mutableStateOf("") }
+                        val speedInteger = if(speed.isNotEmpty()) speed.toInt() else 0
+                        isLottiePlaying = speedInteger >70
+                        SpeedometerLikeProgress(indicatorValue = speedInteger, bigTextFontSize = 35.sp)
+                        Spacer(modifier = Modifier.height(24.dp))
+                        TextField(
+                            value = speed, onValueChange = {
+                                    speed = it
+                            },
+                            keyboardOptions = KeyboardOptions(
+                                keyboardType = KeyboardType.Number,
+                                imeAction = ImeAction.Done,
+                            ),
+                            placeholder = {
+                                Text(text = "Need For Speed...")
+                            }
+                        )
                     }
                 }
             }
---
+++ app/src/main/java/com/example/szfirstapplication/composables/CustomComponent.kt
@@ -0,0 +1,182 @@
+package com.example.szfirstapplication.composables
+
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.animateIntAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun SpeedometerLikeProgress(
+    canvasSize: Dp = 300.dp,
+    indicatorValue: Int = 0,
+    maxIndicatorValue: Int = 100,
+    backgroundIndicatorColor: Color = MaterialTheme.colors.onSurface.copy(alpha = 0.1f),
+    backgroundIndicatorStrokeWidth: Float = 100f,
+    foregroundIndicatorColor: Color = MaterialTheme.colors.primary,
+    foregroundIndicatorStrokeWidth: Float = 100f,
+    bigTextColor: Color = MaterialTheme.colors.onSurface,
+    bigTextFontSize: TextUnit = MaterialTheme.typography.h3.fontSize,
+    bigTextSuffix: String = "mph",
+    smallText: String = "Speed",
+    smallTextColor: Color = MaterialTheme.colors.onSurface.copy(alpha = 0.3f),
+    smallTextFontSize: TextUnit = MaterialTheme.typography.h6.fontSize,
+) {
+    var allowedIndicatorValue by remember { mutableStateOf(maxIndicatorValue) }
+    allowedIndicatorValue = if (indicatorValue <= maxIndicatorValue) {
+        indicatorValue
+    } else {
+        maxIndicatorValue
+    }
+    val resultValue by animateIntAsState(
+        targetValue = allowedIndicatorValue,
+        animationSpec = tween(1000)
+    )
+
+    var progressIndicatorValue by remember { mutableStateOf(0f) }
+    progressIndicatorValue = (allowedIndicatorValue.toFloat())
+    val progressPercentage = (progressIndicatorValue / maxIndicatorValue) * 100
+    val sweepAngle by animateFloatAsState(
+        targetValue = (2.4 * progressPercentage).toFloat(),
+        animationSpec = tween(durationMillis = 1000)
+    )
+
+    val animatedBigTextColor by animateColorAsState(
+        targetValue = if (allowedIndicatorValue == 0)
+            MaterialTheme.colors.onSurface.copy(alpha = 0.3f)
+        else bigTextColor,
+        animationSpec = tween(1000)
+    )
+
+    Column(
+        modifier = Modifier
+            .size(canvasSize)
+            .drawBehind {
+                val componentSize = size / 1.25f
+                backgroundIndicator(
+                    componentSize = componentSize,
+                    indicatorColor = backgroundIndicatorColor,
+                    indicatorStrokeWidth = backgroundIndicatorStrokeWidth
+                )
+                foregroundIndicator(
+                    sweepAngle = sweepAngle,
+                    componentSize = componentSize,
+                    indicatorColor = foregroundIndicatorColor,
+                    indicatorStrokeWidth = foregroundIndicatorStrokeWidth
+                )
+            },
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        EmbeddedElements(
+            bigText = resultValue.toString(),
+            bigTextColor = animatedBigTextColor,
+            bigTextFontSize = bigTextFontSize,
+            bigTextSuffix = bigTextSuffix,
+            smallText = smallText,
+            smallTextColor = smallTextColor,
+            smallTextFontSize = smallTextFontSize
+        )
+    }
+}
+
+
+fun DrawScope.backgroundIndicator(
+    componentSize: Size,
+    indicatorColor: Color,
+    indicatorStrokeWidth: Float
+) {
+    drawArc(
+        size = componentSize,
+        color = indicatorColor,
+        startAngle = 150f,
+        sweepAngle = 240f,
+        useCenter = false,
+        style = Stroke(
+            width = indicatorStrokeWidth,
+            cap = StrokeCap.Round
+        ),
+        topLeft = Offset(
+            x = (size.width - componentSize.width) / 2f,
+            y = (size.height - componentSize.height) / 2f,
+        )
+
+    )
+}
+
+
+fun DrawScope.foregroundIndicator(
+    sweepAngle: Float,
+    componentSize: Size,
+    indicatorColor: Color,
+    indicatorStrokeWidth: Float
+) {
+    drawArc(
+        size = componentSize,
+        color = indicatorColor,
+        startAngle = 150f,
+        sweepAngle = sweepAngle,
+        useCenter = false,
+        style = Stroke(
+            width = indicatorStrokeWidth,
+            cap = StrokeCap.Round
+        ),
+        topLeft = Offset(
+            x = (size.width - componentSize.width) / 2f,
+            y = (size.height - componentSize.height) / 2f,
+        )
+
+    )
+}
+
+@Composable
+fun EmbeddedElements(
+    bigText: String,
+    bigTextColor: Color,
+    bigTextFontSize: TextUnit,
+    bigTextSuffix: String,
+    smallText: String,
+    smallTextColor: Color,
+    smallTextFontSize: TextUnit,
+) {
+    Text(
+        text = smallText,
+        color = smallTextColor,
+        fontSize = smallTextFontSize,
+        textAlign = TextAlign.Center
+    )
+    Text(
+        text = "$bigText $bigTextSuffix",
+        color = bigTextColor,
+        fontSize = bigTextFontSize,
+        textAlign = TextAlign.Center,
+        fontWeight = FontWeight.Bold
+    )
+}
+
+@Composable
+@Preview(showBackground = true)
+fun SpeedometerLikeProgressPreview() {
+    SpeedometerLikeProgress(bigTextFontSize = 30.sp)
+}
